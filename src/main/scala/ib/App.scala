f8de1f1a9add7a359275c767c45b09b46f93263b
liken <ken.liqi@gmail.com>
2015-08-10 22:11:50 +0100

    add pi example

---
+++ src/main/scala/ib/App.scala
@@ -0,0 +1,25 @@
+package ib
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+
+/**
+ * Hello world!
+ *
+ */
+object App {
+  def main (args: Array[String]) {
+    val conf = new SparkConf().setAppName("Calculate Pi")
+    conf.set("spark.master", "local[2]")
+    val spark = new SparkContext(conf)
+    val n = 9999999
+    val pi : Double = (spark.parallelize(1 to n, 2) map {
+      i => {
+        val x = Math.random * 2 -1
+        val y = Math.random * 2 -1
+        if(x*x+y*y<=1) 1 else 0
+      }
+    } reduce(_+_) )* 4.0 / n
+    println(s"The Pi is $pi")
+  }
+}
